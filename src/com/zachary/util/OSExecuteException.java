cebbbb23b3a3908959622a9864fdce84ba87eed3
zacharyzzl <1015989118@qq.com>
2020-05-30 16:53:21 +0800

    进程控制Process ProcessBuilder

---
+++ src/com/zachary/util/OSExecuteException.java
@@ -0,0 +1,17 @@
+package com.zachary.util;
+
+/**
+ * @author Zachary.Zheng
+ * @version 1.0
+ * @date 2020年5月30日 下午4:12:26
+ */
+public class OSExecuteException extends RuntimeException{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public OSExecuteException(String message) {
+		super(message);
+	}
+}
